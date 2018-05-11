13833486720a52355a776d7dd29edb94bdbe63e6
wenjin <wenjin@wenjins-Mac-mini.local>
2018-05-11 09:51:47 +0800

    删除文件

--- src/main/java/com/artifex/mupdfdemo/PageView.java
+++ src/main/java/com/artifex/mupdfdemo/PageView.java
@@ -16,9 +16,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
-
-import com.jiajian.obor.R;
-
 import java.util.ArrayList;
 import java.util.Iterator;
 
